94896c47fc5361799ba96ae5c187544b40b0c61a
Justin Newberry <jnewb@umich.edu>
2020-09-05 00:50:52 +0100

    cleanup, use libcomm rest client

--- src/AprilTagDetector.cpp
+++ src/AprilTagDetector.cpp
@@ -17,13 +17,13 @@ AprilTagDetector::AprilTagDetector(double fx_in, double fy_in, double cx_in, dou
 	//td->refine_edges = 1;
 	td->nthreads = 2;
 
-	vision.field_position.x = 0;
-	vision.field_position.y = 0;
-	vision.field_position.yaw = 0;
+	vision._field_position.set_x(0);
+	vision._field_position.set_y(0);
+	vision._field_position.set_yaw(0);
 
-	moving_average.x = 0;
-	moving_average.y = 0;
-	moving_average.yaw = 0;
+	moving_average.set_x( 0);
+	moving_average.set_y( 0);
+	moving_average.set_yaw( 0);
 }
 
 AprilTagDetector::~AprilTagDetector()
@@ -34,12 +34,10 @@ AprilTagDetector::~AprilTagDetector()
 
 void get_position_from_pose(apriltag_pose_t pose, TagPosition *position)
 {
-	position->x = pose.t->data[0] * 39.3701; // * 86.6595 - .621;
-	// flip x
-	position->x = -position->x;
+	position->set_x(-pose.t->data[0] * 39.3701); // * 86.6595 - .621;
 
-	position->y = pose.t->data[1] * 39.3701; // * 86.6595 - .621;
-	position->z = pose.t->data[2] * 39.3701; // * 86.6595 - .621;
+	position->set_y(pose.t->data[1] * 39.3701); // * 86.6595 - .621;
+	position->set_z(pose.t->data[2] * 39.3701); // * 86.6595 - .621;
 
 	double r11 = pose.R->data[0];
 	double r12 = pose.R->data[1];
@@ -51,34 +49,33 @@ void get_position_from_pose(apriltag_pose_t pose, TagPosition *position)
 	double r32 = pose.R->data[7];
 	double r33 = pose.R->data[8];
 
-	position->roll = atan2(r21, r11) / (2 * 3.14) * 360;
+	position->set_roll(atan2(r21, r11) / (2 * 3.14) * 360);
 
-	position->yaw = atan2(-r31, sqrt(r32 * r32 + r33 * r33)) / (2 * 3.14) * 360;
+	position->set_yaw (-atan2(-r31, sqrt(r32 * r32 + r33 * r33)) / (2 * 3.14) * 360);
 
-	// flip y
-	position->yaw = -position->yaw;
 
-	position->pitch = atan2(r32, r33) / (2 * 3.14) * 360;
+
+	position->set_pitch(atan2(r32, r33) / (2 * 3.14) * 360);
 }
 
 void get_fieldposition_estimate(TagPosition tag, float tag_displacement, FieldPosition *fp)
 {
 	float tag_distance = 23.5; // displacement from center
-	float tag_theta = -tag.yaw * (2 * 3.14) / 360.0;
+	float tag_theta = -tag.get_yaw() * (2 * 3.14) / 360.0;
 
-	float tag_x_1 = sin(tag_theta) * tag.z;
-	float tag_x_2 = sin(3.14 / 2.0 - tag_theta) * tag.x;
+	float tag_x_1 = sin(tag_theta) * tag.get_z();
+	float tag_x_2 = sin(3.14 / 2.0 - tag_theta) * tag.get_x();
 
-	float tag_y_1 = cos(-tag_theta) * tag.z;
-	float tag_y_2 = cos(3.14 / 2.0 + tag_theta) * tag.x;
+	float tag_y_1 = cos(-tag_theta) * tag.get_z();
+	float tag_y_2 = cos(3.14 / 2.0 + tag_theta) * tag.get_x();
 
 	float tag_x_estimate = tag_x_1 + tag_x_2 - tag_displacement;
 	float tag_y_estimate = tag_y_1 + tag_y_2;
-	float tag_yaw_estimate = tag.yaw;
+	float tag_yaw_estimate = tag.get_yaw();
 
-	fp->x = tag_x_estimate;
-	fp->y = tag_y_estimate;
-	fp->yaw = tag_yaw_estimate;
+	fp->set_x ( tag_x_estimate);
+	fp->set_y( tag_y_estimate);
+	fp->set_yaw ( tag_yaw_estimate);
 }
 
 void AprilTagDetector::detect(Mat &gray)
@@ -95,22 +92,22 @@ void AprilTagDetector::detect(Mat &gray)
 	comm::TagPosition tag1_pos2;
 
 	// reset tag poses
-	tag0_pos1.x = 0;
-	tag0_pos1.y = 0;
-	tag0_pos1.z = 0;
-	tag0_pos1.yaw = 0;
-	tag1_pos1.x = 0;
-	tag1_pos1.y = 0;
-	tag1_pos1.z = 0;
-	tag1_pos1.yaw = 0;
-	tag0_pos2.x = 0;
-	tag0_pos2.y = 0;
-	tag0_pos2.z = 0;
-	tag0_pos2.yaw = 0;
-	tag1_pos2.x = 0;
-	tag1_pos2.y = 0;
-	tag1_pos2.z = 0;
-	tag1_pos2.yaw = 0;
+	tag0_pos1.set_x( 0);
+	tag0_pos1.set_y ( 0);
+	tag0_pos1.set_z ( 0);
+	tag0_pos1.set_yaw ( 0);
+	tag1_pos1.set_x ( 0);
+	tag1_pos1.set_y ( 0);
+	tag1_pos1.set_z ( 0);
+	tag1_pos1.set_yaw ( 0);
+	tag0_pos2.set_x ( 0);
+	tag0_pos2.set_y ( 0);
+	tag0_pos2.set_z ( 0);
+	tag0_pos2.set_yaw ( 0);
+	tag1_pos2.set_x ( 0);
+	tag1_pos2.set_y ( 0);
+	tag1_pos2.set_z ( 0);
+	tag1_pos2.set_yaw ( 0);
 
 		bool pose_valid = false;
 
@@ -167,123 +164,123 @@ void AprilTagDetector::detect(Mat &gray)
 	get_fieldposition_estimate(tag1_pos1, -24.0 / 2.0, &tag1_pos1_fp);
 	get_fieldposition_estimate(tag1_pos2, -24 / 2.0, &tag1_pos2_fp);
 
-	//cout << "0: " << tag0_pos1_fp.x << " " << tag0_pos2_fp.x << endl;
-	// cout << "1: " << tag1_pos1_fp.x << " " << tag1_pos2_fp.x << endl;
+	//cout << "0: " << tag0_pos1_fp.set_x( << " " << tag0_pos2_fp.set_x( << endl;
+	// cout << "1: " << tag1_pos1_fp.set_x( << " " << tag1_pos2_fp.set_x( << endl;
 
 	// try all pairs of tags and poses to find best match
 
 	// pose1_2 -> tag0pos1, tag1_pos2
-	int pose1_1 = abs(tag0_pos1_fp.x - tag1_pos1_fp.x) + abs(tag0_pos1_fp.y - tag1_pos1_fp.y);
-	int pose1_2 = abs(tag0_pos1_fp.x - tag1_pos2_fp.x) + abs(tag0_pos1_fp.y - tag1_pos2_fp.y);
-	int pose2_1 = abs(tag0_pos2_fp.x - tag1_pos1_fp.x) + abs(tag0_pos2_fp.y - tag1_pos1_fp.y);
-	int pose2_2 = abs(tag0_pos2_fp.x - tag1_pos2_fp.x) + abs(tag0_pos2_fp.y - tag1_pos2_fp.y);
+	int pose1_1 = abs(tag0_pos1_fp.get_x() - tag1_pos1_fp.get_x()) + abs(tag0_pos1_fp.get_y() - tag1_pos1_fp.get_y());
+	int pose1_2 = abs(tag0_pos1_fp.get_x() - tag1_pos2_fp.get_x()) + abs(tag0_pos1_fp.get_y() - tag1_pos2_fp.get_y());
+	int pose2_1 = abs(tag0_pos2_fp.get_x() - tag1_pos1_fp.get_x()) + abs(tag0_pos2_fp.get_y() - tag1_pos1_fp.get_y());
+	int pose2_2 = abs(tag0_pos2_fp.get_x() - tag1_pos2_fp.get_x()) + abs(tag0_pos2_fp.get_y() - tag1_pos2_fp.get_y());
 
 	std::vector<int> pose_errors = {pose1_1, pose1_2, pose2_1};
-	vision.tag0.x = tag0_pos1.x;
-	vision.tag0.y = tag0_pos1.y;
-	vision.tag0.z = tag0_pos1.z;
-	vision.tag0.yaw = tag0_pos1.yaw;
-	vision.tag1.x = tag1_pos1.x;
-	vision.tag1.y = tag1_pos1.y;
-	vision.tag1.z = tag1_pos1.z;
-	vision.tag1.yaw = tag1_pos1.yaw;
+	vision._tag0.set_x( tag0_pos1.get_x());
+	vision._tag0.set_y ( tag0_pos1.get_y());
+	vision._tag0.set_z( tag0_pos1.get_z());
+	vision._tag0.set_yaw( tag0_pos1.get_yaw());
+	vision._tag1.set_x( tag1_pos1.get_x());
+	vision._tag1.set_y (tag1_pos1.get_y());
+	vision._tag1.set_z ( tag1_pos1.get_z());
+	vision._tag1.set_yaw ( tag1_pos1.get_yaw());
 
-	vision.field_position.x =   0;
-		vision.field_position.y =   0;
-		vision.field_position.yaw = 0;
+	vision._field_position.set_x ( 0);
+		vision._field_position.set_y (   0);
+		vision._field_position.set_yaw ( 0);
 
 	
 
 	
 
 	// both tags are visible
-	if (tag0_pos1.z != 0 && tag1_pos1.z != 0)
+	if (tag0_pos1.get_z() != 0 && tag1_pos1.get_z() != 0)
 	{
 		int idx = std::min_element(pose_errors.begin(), pose_errors.end()) - pose_errors.begin();
 		//cout << idx << endl;
-			//cout << "0: " << tag0_pos1_fp.x << " " << tag0_pos2_fp.x << endl;
-			//cout << "1: " << tag1_pos1_fp.x << " " << tag1_pos2_fp.x << endl;
+			//cout << "0: " << tag0_pos1_fp.set_x( << " " << tag0_pos2_fp.set_x( << endl;
+			//cout << "1: " << tag1_pos1_fp.set_x( << " " << tag1_pos2_fp.set_x( << endl;
 			
 			// pose1_1 had min error
 			if (idx == 0)
 			{
-				cur_fp.x = (tag0_pos1_fp.x + tag1_pos1_fp.x) / 2.0;
-				cur_fp.y = (tag0_pos1_fp.y + tag1_pos1_fp.y) / 2.0;
-				cur_fp.yaw = (tag0_pos1_fp.yaw + tag1_pos1_fp.yaw) / 2.0;
+				cur_fp.set_x(  (tag0_pos1_fp.get_x() + tag1_pos1_fp.get_x()) / 2.0);
+				cur_fp.set_y(  (tag0_pos1_fp.get_y() + tag1_pos1_fp.get_y()) / 2.0);
+				cur_fp.set_yaw( (tag0_pos1_fp.get_yaw() + tag1_pos1_fp.get_yaw()) / 2.0);
 			}
 			// pose1_2
 			else if (idx == 1)
 			{
-				cur_fp.x = (tag0_pos1_fp.x + tag1_pos2_fp.x) / 2.0;
-				cur_fp.y = (tag0_pos1_fp.y + tag1_pos2_fp.y) / 2.0;
-				cur_fp.yaw = (tag0_pos1_fp.yaw + tag1_pos2_fp.yaw) / 2.0;
+				cur_fp.set_x(  (tag0_pos1_fp.get_x() + tag1_pos2_fp.get_x()) / 2.0);
+				cur_fp.set_y(  (tag0_pos1_fp.get_y() + tag1_pos2_fp.get_y()) / 2.0);
+				cur_fp.set_yaw( (tag0_pos1_fp.get_yaw() + tag1_pos2_fp.get_yaw()) / 2.0);
 			}
 			// pose2_1
 			else if (idx == 2)
 			{
-				cur_fp.x = (tag0_pos2_fp.x + tag1_pos1_fp.x) / 2.0;
-				cur_fp.y = (tag0_pos2_fp.y + tag1_pos1_fp.y) / 2.0;
-				cur_fp.yaw = (tag0_pos2_fp.yaw + tag1_pos1_fp.yaw) / 2.0;
+				cur_fp.set_x(  (tag0_pos2_fp.get_x() + tag1_pos1_fp.get_x()) / 2.0);
+				cur_fp.set_y(  (tag0_pos2_fp.get_y() + tag1_pos1_fp.get_y()) / 2.0);
+				cur_fp.set_yaw( (tag0_pos2_fp.get_yaw() + tag1_pos1_fp.get_yaw()) / 2.0);
 			}
 	
 		
 	}
-	else if (vision.tag0.z != 0)
+	else if (vision._tag0.get_z() != 0)
 	{
-		cur_fp.x = tag0_pos1_fp.x;
-		cur_fp.y = tag0_pos1_fp.y;
-		cur_fp.yaw = tag0_pos1_fp.yaw;
+		cur_fp.set_x(  tag0_pos1_fp.get_x());
+		cur_fp.set_y(  tag0_pos1_fp.get_y());
+		cur_fp.set_yaw( tag0_pos1_fp.get_yaw());
 	}
-	else if (vision.tag1.z != 0)
+	else if (vision._tag1.get_z() != 0)
 	{
-		cur_fp.x = tag1_pos1_fp.x;
-		cur_fp.y = tag1_pos1_fp.y;
-		cur_fp.yaw = tag1_pos1_fp.yaw;
+		cur_fp.set_x( tag1_pos1_fp.get_x());
+		cur_fp.set_y(  tag1_pos1_fp.get_y());
+		cur_fp.set_yaw(  tag1_pos1_fp.get_yaw());
 	}
 	else
 	{
-		vision.tag0.x = 0;
-		vision.tag0.y = 0;
-		vision.tag0.z = 0;
-		vision.tag0.yaw = 0;
-		vision.tag1.x = 0;
-		vision.tag1.y = 0;
-		vision.tag1.z = 0;
-		vision.tag1.yaw = 0;
+		vision._tag0.set_x(0);
+		vision._tag0.set_y(  0);
+		vision._tag0.set_z(  0);
+		vision._tag0.set_yaw(  0);
+		vision._tag1.set_x  (0);
+		vision._tag1.set_y  (0);
+		vision._tag1.set_z  (0);
+		vision._tag1.set_yaw(  0);
 	}
 	
 	if(pose_valid){
 		
-		int diff = abs(moving_average.x - cur_fp.x) + abs(moving_average.y - cur_fp.y);
+		int diff = abs(moving_average.get_x() - cur_fp.get_x()) + abs(moving_average.get_y() - cur_fp.get_y());
 
 		if(diff < 30){
-			moving_average.x = (alpha * cur_fp.x) + (1.0 - alpha) * moving_average.x;
-			moving_average.y = (alpha * cur_fp.y) + (1.0 - alpha) * moving_average.y;
-			moving_average.yaw = (alpha * cur_fp.yaw) + (1.0 - alpha) * moving_average.yaw;
+			moving_average.set_x ( (alpha * cur_fp.get_x() + (1.0 - alpha) * moving_average.get_x()));
+			moving_average.set_y ( (alpha * cur_fp.get_y() + (1.0 - alpha) * moving_average.get_y()));
+			moving_average.set_yaw( (alpha * cur_fp.get_yaw() + (1.0 - alpha) * moving_average.get_yaw()));
 			reset_times = 0;
 		}
 		else if(reset_times > 5){
 			
-			moving_average.x = cur_fp.x;
-			moving_average.y = cur_fp.y;
-			moving_average.yaw = cur_fp.yaw;
+			moving_average.set_x( cur_fp.get_x());
+			moving_average.set_y( cur_fp.get_y());
+			moving_average.set_yaw (cur_fp.get_yaw());
 		}
 		else{
 			reset_times++;
 		}
 
-		vision.field_position.x =   moving_average.x;
-		vision.field_position.y =   moving_average.y;
-		vision.field_position.yaw = moving_average.yaw;
+		vision._field_position.set_x(   moving_average.get_x());
+		vision._field_position.set_y(    moving_average.get_y());
+		vision._field_position.set_yaw ( moving_average.get_yaw());
 	}
 
-	vision.vision_good = pose_valid ? 1 : 0;
+	vision.set_vision_good( pose_valid ? 1 : 0);
 }
 
 std::ostream &operator<<(std::ostream &os, const comm::TagPosition &t)
 {
-	os << t.x << " " << t.y << " " << t.z << " ";
-	os << t.yaw << " " << t.pitch << " " << t.roll << "\r\n"
-	   << std::flush;
+	//os << t.get_x() << " " << t.get_y() << " " << t.get_z() << " ";
+	//os << t.get_yaw() << " " << t.get_pitch() << " " << t.get_roll() << "\r\n"
+	//   << std::flush;
 	return os;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@
 #include <mutex>
 #include <deque>
 
-#include "TcpClient.h"
+#include <comm/RESTClient.h>
 
 #include <comm/Vision.h>
+#include <comm/Vision_Image.h>
 #include <comm/Identifier.h>
-//#include "Servo.h"
-//#include "PositionROSPublisher.h"
+#include <comm/Vision_Command.h>
 
 using namespace std;
 using namespace cv;
@@ -29,7 +29,9 @@ class VisionMain
 	std::mutex mtx;
 	std::vector<uchar> buffer;
 
-	TcpClient client;
+	comm::RESTClient client;
+
+	int auto_quality = 2;
 
 	//Servo servo;
 
@@ -38,20 +40,23 @@ class VisionMain
 		// send image to dashboard
 		std::vector<int> param(2);
 		param[0] = cv::IMWRITE_JPEG_QUALITY;
-		param[1] = 2;
+		param[1] = auto_quality;
 		cv::imencode(".jpg", img, buffer, param);
-		//cout << buffer.size() << endl;
+
+		if(buffer.size() > 8191 * 0.8){
+			auto_quality -= 3;
+		}
+		else{
+			auto_quality += 1;
+		}
+		buffer.resize(8191);
 
 
-		vector<uint8_t> type(1);
-		type[0] = (uint8_t)comm::CommunicationDefinitions::TYPE::VISION_IMAGE;
+		std::unique_ptr<comm::RESTPacket> vision_image_packet(new comm::Vision_Image);
 
-		int sz = comm::CommunicationDefinitions::PACKET_SIZES.at(comm::CommunicationDefinitions::TYPE::VISION_IMAGE) + 1;
-		//cout << sz << endl;
-		std::vector<uchar> buf(sz);
-		buf[0] = (char)comm::CommunicationDefinitions::TYPE::VISION_IMAGE;
-		std::copy(buffer.begin(), buffer.end(), buf.begin() + 1);
-		client.write(buf);
+		((comm::Vision_Image*)vision_image_packet.get())->set_image((char*)&buffer[0]);
+		client.send_message(vision_image_packet.get());
+
 	}
 
 	void camera_worker()
@@ -72,7 +77,7 @@ class VisionMain
 	}
 
 public:
-	VisionMain()
+	VisionMain() : client(comm::CommunicationDefinitions::IDENTIFIER::VISION)
 	{
 	}
 	void run()
@@ -123,71 +128,54 @@ public:
 
 		double angle = 0;
 
+		bool enable_streaming = false;
+		bool enable_detection = true;
+
 		while (true)
 		{
-			//servo.write(angle+=.25);
-			if (true)
-			{
-				std::lock_guard<std::mutex> lock(mtx);
+		if(true){
+					std::lock_guard<std::mutex> lock(mtx);
 
-				cvtColor(img, gray, cv::COLOR_RGB2GRAY);
+					cvtColor(img, gray, cv::COLOR_RGB2GRAY);
+		}
+
+			if(enable_detection){
+				det.detect(gray);
+				client.send_message(&det.vision);
+			}
+			
+			if (enable_streaming)
+			{
+				send_image(gray);
 			}
 
-			send_image(gray);
-
-			if (client.read_nonblocking(recv, 3))
-			{
-				bool valid_key = true;
-				for (int i = 0; i < 3; i++)
-				{
-					if (recv[i] != comm::CommunicationDefinitions::key[i])
-					{
-						valid_key = false;
+		
+			auto messages = client.get_messages();
+			for(auto &m : messages){
+				if(m->type() == comm::CommunicationDefinitions::TYPE::VISION_COMMAND){
+					comm::Vision_Command* command = (comm::Vision_Command*)m.get();
+					if(command->get_command() == 5){
+						send_image(gray);
+						enable_streaming = false;
 					}
-				}
+					if(command->get_command() == 6){
+						enable_streaming = true;
+					}
+					if(command->get_command() == 7){
+						enable_streaming = false;
+					}
 
-				if (valid_key)
-				{
-					cout << "valid Key" << endl;
-
-					if (client.read_nonblocking(recv, 128))
-					{
-						cout << (int)recv[0] << endl;
-						if (recv[0] == (char)comm::CommunicationDefinitions::TYPE::VISION_COMMAND)
-						{
-							if (recv[1] == 0)
-							{
-								cout << "SEnding image" << endl;
-								send_image(gray);
-							}
-							else if (recv[1] == 1)
-							{
-								cout << "Setting Vision Properties" << endl;
-								double exposure = *(short *)(recv + 1) / 100.0;
-								int gain = (int)recv[5];
-								cout << exposure << " ";
-								cout << gain << " ";
-								cout << cap.set(CAP_PROP_AUTO_EXPOSURE, .75);
-								cout << cap.set(CAP_PROP_EXPOSURE, exposure);
-								cout << cap.set(CAP_PROP_GAIN, gain);
-								cout << endl;
-							}
-						}
+					if(command->get_command() == 8){
+						enable_detection = true;
+					}
+					if(command->get_command() == 9){
+						enable_detection = false;
 					}
 				}
 			}
+			
 
-			det.detect(gray);
 
-			//std::cout << det.vision.tag0 << std::endl;
-
-			//cv::imshow("Captured", img);
-			//cv::waitKey(1);
-			auto data = det.vision.Serialize();
-
-			client.write(data);
-
-			//std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
 	}
 };
